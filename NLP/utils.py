64c652aa6ccb8f57b3ef34a0ed27b88940a7989e
Christian Urcuqui <ulcamilo@gmail.com>
2021-11-17 15:18:09 -0500

    updated

--- NLP/utils.py
+++ NLP/utils.py
@@ -11,6 +11,8 @@ from nltk.tokenize import TweetTokenizer
 
 from matplotlib.patches import Ellipse
 import matplotlib.transforms as transforms
+import pandas as pd
+
 
 def process_tweet(tweet):
     """Process tweet function.
@@ -184,6 +186,40 @@ def lookup(freqs, word, label):
         n = freqs[pair]
 
     return n
+
+def get_dict(file_name):
+    """
+    This function returns the english to french dictionary given a file where the each column corresponds to a word.
+    Check out the files this function takes in your workspace.
+    """
+    my_file = pd.read_csv(file_name, delimiter=' ')
+    etof = {}  # the english to french dictionary to be returned
+    for i in range(len(my_file)):
+        # indexing into the rows.
+        en = my_file.loc[i][0]
+        fr = my_file.loc[i][1]
+        etof[en] = fr
+
+    return etof
+
+def cosine_similarity(A, B):
+    '''
+    Input:
+        A: a numpy array which corresponds to a word vector
+        B: A numpy array which corresponds to a word vector
+    Output:
+        cos: numerical number representing the cosine similarity between A and B.
+    '''
+    # you have to set this variable to the true label.
+    cos = -10
+    dot = np.dot(A, B)
+    norma = np.linalg.norm(A)
+    normb = np.linalg.norm(B)
+    cos = dot / (norma * normb)
+
+    return cos
+
+
 
 
 def get_vectors(embeddings, words):
